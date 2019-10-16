01753ee1a39d7873224532a29df69564e601ecdf
VolodymyrTrach <vovka.trach@gmail.com>
2019-10-16 23:03:51 +0300

    created http service

--- fun-football/src/app/app.component.ts
+++ fun-football/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {GetDataService} from "../../servises/get-data.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'fun-football';
+  teamLogo: object;
+  constructor (public httpServise: GetDataService){  }
+  ngOnInit(): void {
+    this.httpServise.getTeam().subscribe(res=>{
+      console.log(res.api.teams[0].logo)
+      this.teamLogo = res.api.teams[0].logo;
+      // this.teamLogo = res[api];
+    })
+  }
 }
